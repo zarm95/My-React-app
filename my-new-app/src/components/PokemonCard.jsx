bfdb6986fc172a03b3f73f86c9a300c4b66e5231
yahia <yahiamarzouk1995@gmail.com>
2023-03-28 11:34:40 +0200

    deuxieme challeng

--- my-new-app/src/components/PokemonCard.jsx
+++ my-new-app/src/components/PokemonCard.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const PokemonCard = () => {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  let displayPoke = (pokemon) => {
+    return pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>...</p>;
+  };
+  let pokemon = pokemonList[0];
   return (
     <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-        <figcaption>bulbasaur</figcaption>
+      {displayPoke(pokemon)}
+      <figcaption>{pokemon.name}</figcaption>;
     </figure>
-  )
-}
+  );
+};
 
-export default PokemonCard
+export default PokemonCard;
