2f85eeb8ac1b2777db3b12541a298f712340bb7c
zarm95 <59035854+zarm95@users.noreply.github.com>
2023-03-28 17:01:00 +0200

    Update NavBar.jsx

--- my-new-app/src/components/NavBar.jsx
+++ my-new-app/src/components/NavBar.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 
-const NavBar = ({ hendlePrecedent, hendleSuivant }) => {
+const NavBar = ({ pokemonList, pokemonIndex, setPokemonIndex }) => {
+  const hendleSuivant = () => {
+    if (pokemonIndex >= pokemonList.length - 1) {
+      alert("index superieur");
+    } else {
+      setPokemonIndex(pokemonIndex + 1);
+    }
+  };
+  const hendlePrecedent = () => {
+    if (pokemonIndex <= 0) {
+      alert("index inferieur");
+    } else {
+      setPokemonIndex(pokemonIndex - 1);
+    }
+  };
+
   return (
     <div>
       <button onClick={hendleSuivant}>suivant </button>
