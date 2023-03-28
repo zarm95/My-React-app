43d27f384140f73f1c0bf799fdf7d0608ffead93
yahia <yahiamarzouk1995@gmail.com>
2023-03-28 12:09:01 +0200

    exercice 1 sur les props

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -1,12 +1,22 @@
-import PokemonCard from "./components/PokemonCard"
+import PokemonCard from "./components/PokemonCard";
 function App() {
-   
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  let pokemon = pokemonList[1];
+
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemon} />
     </div>
-  )
-     
+  );
 }
 
-export default App
+export default App;
