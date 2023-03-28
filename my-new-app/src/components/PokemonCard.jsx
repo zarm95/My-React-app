b7d31f1a7e5385e28193a324e3cdbfcdca54ef28
yahia <yahiamarzouk1995@gmail.com>
2023-03-28 10:37:19 +0200

    composant 1 pokemoncard

---
+++ my-new-app/src/components/PokemonCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PokemonCard = () => {
+  return (
+    <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+        <figcaption>bulbasaur</figcaption>
+    </figure>
+  )
+}
+
+export default PokemonCard
